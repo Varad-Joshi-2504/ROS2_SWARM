e102e12d3d7b74b25692b76d3bd640d8d277e92a
Varad Joshi <varadmandar2504@gmail.com>
2022-12-29 11:35:12 +0530

    new commit

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 - Create a drone
 - Add a solid model for the drone for simulation
 - to be continued
+
+    task1:
+    1.rectagle of length w and height h
+    2.another rec of len w1 and height h1 such that :
+           w1<w-max of turtle hieght and width>
+           h1<h-max of turtle hieght and width>
+    3.MOVE around in First rec big , dont enter smaller rec
+    4.smaller rec is random placed 
--- src/my_robot_controller/my_robot_controller/turtle_controller.py
+++ src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -5,6 +5,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+import random
 
 class TurtleController(Node):
 
@@ -13,12 +14,19 @@ class TurtleController(Node):
         self.cmd_vel_publisher_=self.create_publisher(Twist,'/turtle1/cmd_vel',10)
         self.pose_subscriber_=self.create_subscription(Pose,'/turtle1/pose',self.callback,10)
         self.get_logger().info('...')
+
+
+    def next_x(self,x,left,right):
+        """return the next value of x"""
+        return random.uniform(0.5, 3)
+        
+
     
     def callback(self , pose:Pose):
         msg = Twist()
         
         if pose.x > 9 or pose.y > 9 or pose.x < 2 or pose.y < 2:
-            msg.linear.x=1.0
+            msg.linear.x=self.next_x(pose.x , 2, 9)
             msg.linear.y=0.0
             msg.angular.z=0.9
 
@@ -37,3 +45,4 @@ def main(args=None):
     node = TurtleController()
     rclpy.spin(node)
     rclpy.shutdown()
+
