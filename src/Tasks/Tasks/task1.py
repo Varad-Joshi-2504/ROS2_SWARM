4791930a3b5a04ee4b303333787db54813962c09
Varad Joshi <varadmandar2504@gmail.com>
2023-01-04 16:53:15 +0530

    task1_new

--- src/Tasks/Tasks/task1.py
+++ src/Tasks/Tasks/task1.py
@@ -6,106 +6,137 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import random
+import math
+import argparse
 
-class Task1(Node):
+# Y:
+# up/top - values increase
+# down/bottom - values decrease
 
-    def  __init__(self):
-        super().__init__('task1')
-        # self.w = float(input("enter length of outer rectangle : "))    #this block takes the width and height of outer rectangle
-        # self.h = float(input("enter height of outer rectangle : "))
-        # self.c1 = float(input("enter x axis center of outer rectangle : "))
-        # self.c2 = float(input("enter y axis center of outer rectangle : "))
-        # self.left= self.c1 - (self.w / 2.0)
-        # self.top= self.c2 + (self.h / 2.0)
-        # self.right=self.c1+ (self.w / 2.0)
-        # self.down=self.c2 - (self.h / 2.0)
+# X:
+# right - values increase
+# left - values decrease
 
-        # self.w1 = float(input("enter length of inner rectangle : "))  #this block takes the width and height of inner rectangle   [don't use thsi block some problem here]
-        # self.h1 = float(input("enter height of innner rectangle : "))
-        # self.c11 = float(input("enter x axis center of inner rectangle : "))
-        # self.c21 = float(input("enter y axis center of inner rectangle : "))
-        # self.left1= self.c11 - (self.w1 / 2.0)
-        # self.top1= self.c21 + (self.h / 2.0)
-        # self.right1=self.c11+ (self.w1 / 2.0)
-        # self.down1=self.c21 - (self.h1 / 2.0)
+left = float(input("left : "))  # outer
+top = float(input("top : "))
+right = float(input("right :"))
+down = float(input("down : "))
+left1 = float(input("left1 : "))  # inner
+top1 = float(input("top1 : "))
+right1 = float(input("right1 : "))
+down1 = float(input("down1 : "))
+print(" outer rec : left : " + str(left) + " right : " + str(right) +
+      " top : " + str(top) + " down : " + str(down) + "\n")
+print(" inner rec : left : " + str(left1) + " right : " + str(right1) +
+      " top : " + str(top1) + " down : " + str(down1))
+# pose_danger_x:0.0
+# pose_danger_y=0.0
 
-        self.left= float(input("left : "))
-        self.top= float(input("top : "))
-        self.right=float(input("right :"))
-        self.down=float(input("down : "))
 
-        self.left1= float(input("left1 : "))
-        self.top1= float(input("top1 : "))
-        self.right1=float(input("right1 : "))
-        self.down1=float(input("down1 : "))
+class TurtleBoundaryLimiter(Node):
 
-        print(" left : " + str(self.left) + " right : " + str(self.right) + " top : " + str(self.top) + " down : " + str(self.down) + "\n")
-        print(" left : " + str(self.left1) + " right : " + str(self.right1) + " top : " + str(self.top1) + " down : " + str(self.down1))
-        self.cmd_vel_publisher_=self.create_publisher(Twist,'/turtle1/cmd_vel',10)
-        self.pose_subscriber_=self.create_subscription(Pose,'/turtle1/pose',self.callback,10)
+    def __init__(self):
+        super().__init__('Turtle_boundary')
+        self.cmd_vel_publisher_ = self.create_publisher(
+            Twist, '/turtle1/cmd_vel', 10)
+        self.pose_subscriber_ = self.create_subscription(
+            Pose, '/turtle1/pose', self.callback, 10)
         self.get_logger().info('...')
-    
-    def next_correction_linear_x(self): #next x value for correction
-        """return the next value of x"""
-        return random.uniform(0.1,0.3)
-        
 
-    def next_correction_linear_y(self): #next y value for correction
-        """return the next value of y"""
-        return random.uniform(0.1,0.3)
-
-    def next_correction_angular_z(self): #next z value for correction
-        """return next value of z"""
-        return 0.9
-    
-    def next_linear_x(self): #next z value for correction
-        """return next value of z"""
-        return random.uniform(3.0,5.0)
-
-    def next_linear_y(self): #next z value for correction
-        """return next value of z"""
-        return random.uniform(3.0,5.0)
-
-        
-    def next_angular_z(self): #next z value for correction
-        """return next value of z"""
-        return 0.0
-
-    
-    def callback(self , pose:Pose):
+    def callback(self, pose: Pose):
         msg = Twist()
-        
-        if pose.x > self.right or pose.y > self.top or pose.x < self.left or pose.y < self.down or (pose.x < self.right1 and pose.x > self.left1 and pose.y < self.top1 and pose.y > self.down1):  #this main if block is for outer rectangle
-            msg.linear.x=self.next_correction_linear_x()
-            msg.linear.y=self.next_correction_linear_y()
-            msg.angular.z=self.next_correction_angular_z()
- 
 
-        else:            #if inside big rec and outside small recc then this:
-            msg.linear.x=self.next_linear_x()
-            msg.linear.y=self.next_linear_y()
-            msg.angular.z=self.next_angular_z()
+        # this is if block for outer and inner limits
 
+        if pose.x > right or pose.y > top or pose.x < left or pose.y < down or (pose.x < right1 and pose.x > left1 and pose.y < top1 and pose.y > down1):
+
+            msg.linear.x = next_correction_linear_x()
+            msg.linear.y = next_correction_linear_y()
+
+           #if pose.theta >
+            msg.angular.z = next_correction_angular_z()
+            # self.warning = warning_callback()
+            # msg.linear.x = next(warning_callback())
+            # msg.linear.y = next(warning_callback())
+            # msg.angular.z = next(warning_callback())
+
+        else:  # if inside big rec and outside small recc then this:
+
+            msg.linear.x = next_linear_x()
+            msg.linear.y = next_linear_y()
+            msg.angular.z = next_angular_z()
+            # self.general = general_movement()
+            # msg.linear.x = next(general_movement())
+            # msg.linear.y = next(general_movement())
+            # msg.angular.z = next(general_movement())
+
+        print (str(msg.linear)+"    "+str(msg.linear.y)+"    "+str(msg.angular.z))
 
         self.cmd_vel_publisher_.publish(msg)
 
 
+# def warning_callback():  # pose:Pose):
+#  #   q = p + pose.linear_velocity*theta
 
-        # Y:
-        # up/top - values increase
-        # down/bottom - values decrease
-
-        # X:
-        # right - values increase
-        # left - values decrease
-
+#     # if (pose.x + pose.linear_velocity*math.sin(pose.theta))==pose_danger_x or (pose.y + pose.linear_velocity*math.cos(pose.theta)):
+#     yield random.uniform(0.1, 0.3)
+#     yield random.uniform(0.1, 0.3)
+#     yield 0.7
 
 
+# def general_movement():
+#     yield random.uniform(3.0, 5.0)
+#     yield random.uniform(3.0, 5.0)
+#     yield random.uniform(0.0, 0.2)
 
 
+def next_correction_linear_x():  # next x value for correction
+    """return the next value of x"""
+    return random.uniform(0.1, 0.3)
 
-def main(args=None):
-    rclpy.init(args=args)
-    node = Task1()
-    rclpy.spin(node)
+
+def next_correction_linear_y():  # next y value for correction
+    """return the next value of y"""
+    return random.uniform(0.1, 0.3)
+
+
+def next_correction_angular_z():  # next z value for correction
+    """return next value of z"""
+    return 0.7
+
+
+def next_linear_x():  # next z value for correction
+    """return next value of z"""
+    return random.uniform(3.0, 5.0)
+
+
+def next_linear_y():  # next z value for correction
+    """return next value of z"""
+    return random.uniform(3.0, 5.0)
+
+
+def next_angular_z():  # next z value for correction
+    """return next value of z"""
+    return random.uniform(0.0, 0.2)
+
+
+def main():
+    # parser = argparse.ArgumentParser(
+    #     prog = 'turtle-limit',
+    #     description = 'Limit the turtle in its track')
+    # parser.add_argument('-l',
+    #                     '--left',
+    #                     action='store')
+    # parser.add_argument('--name',
+    #                     action='store',
+    #                     help='Name of the turtle')
+    # args = parser.parse_args()
+    # print("Turtle ", args.name, " can go at the most ", args.left, " meters to the left")
+    rclpy.init()
+    node = TurtleBoundaryLimiter()
+    rclpy.spin(node.callback)
     rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
